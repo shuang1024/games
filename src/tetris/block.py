dc74c12406131634eafdf8a81418c7000dd11858
Sean Huang <awesome.sean.haha@gmail.com>
2022-06-30 13:03:30 -0700

    Added block class.

---
+++ src/tetris/block.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import *
+
+
+class Block:
+    def __init__(self, type):
+        self.type = type
+        if self.type == 3:
+            self.x = B_WIDTH//2-2
+        else:
+            self.x = B_WIDTH//2-1
+        self.y = 0
+
+    def update_board(self, board):
+        if self.y != 0:
+            board[self.x][self.y] = 7
+        board[self.x][self.y] = self.type
+        print(self.x, self.y)
