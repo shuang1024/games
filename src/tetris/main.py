ef052d7e1ad052fb567f2422280de4070788deb0
Sean Huang <awesome.sean.haha@gmail.com>
2022-06-30 13:05:07 -0700

    Added blocks dropping.

--- src/tetris/main.py
+++ src/tetris/main.py
@@ -1,13 +1,35 @@
-import pygame
 import time
-from .constants import *
+import random
+from copy import deepcopy
+
+import pygame
+
+from constants import *
+from block import *
 
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tetris")
 
+type_col = {0:RED, 1:GREEN, 2:BLUE, 3:YELLOW, 4:MAGENTA, 5:CYAN, 6:ORANGE, 7:BLACK}
+
+def draw(display, board):
+    for i in range(B_WIDTH):
+        for j in range(B_HEIGHT):
+            pygame.draw.rect(display, type_col[board[i][j]], (BORD_SIZE+SQ_SIZE*i, BORD_SIZE+SQ_SIZE*j, SQ_SIZE, SQ_SIZE))
+
 
 def main():
     clock = pygame.time.Clock()
+    board = []
+    for i in range(B_WIDTH):
+        board.append([])
+        for j in range(B_HEIGHT):
+            board[i].append(7)
+
+    curr_block = None
+    start = time.time()
+    display.fill(GRAY)
+    draw(display, board)
 
     while True:
         clock.tick(FPS)
@@ -18,7 +40,23 @@ def main():
                 pygame.quit()
                 return
 
-        display.fill(BLACK)
+
+        if time.time() - start > PAUSE:
+            new_board = deepcopy(board)
+            print(time.time()-start)
+            if not curr_block:
+                curr_block = Block(random.randint(0, 6))
+            else:
+                if curr_block.y == B_HEIGHT-1 or board[curr_block.x][curr_block.y-1] != 7:
+                    curr_block = None
+                    continue
+
+            curr_block.y += 1
+            curr_block.update_board(new_board)
+            start = time.time()
+            display.fill(GRAY)
+            draw(display, new_board)
+            new_board = board
 
 
 main()
