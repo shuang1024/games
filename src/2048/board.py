6779f4a4157ee96c900de195e4a3237c1da56ddf
Sean Huang <awesome.sean.haha@gmail.com>
2023-01-01 10:40:56 -0800

    Added basic board class.

---
+++ src/2048/board.py
@@ -0,0 +1,52 @@
+import random
+import math
+
+import pygame
+
+from constants import *
+
+
+class Board:
+    def __init__(self):
+        self.board = []
+        self.reset_board()
+
+        x1 = random.randint(0, 3)
+        y1 = random.randint(0, 3)
+        x2 = random.randint(0, 3)
+        y2 = random.randint(0, 3)
+        while (x1, y1) == (x2, y2):
+            x2 = random.randint(0, 3)
+            y2 = random.randint(0, 3)
+        self.board[y1][x1] = random.choice(("2", "4"))
+        self.board[y2][x2] = "2"
+        
+
+    def reset_board(self):
+        self.board = []
+        for i in range(4):
+            self.board.append([])
+            for j in range(4):
+                self.board[i].append("")
+
+    def text_block_loc(self, text, i, j):
+        x = j*(SQ_SIZE + PADDING) + PADDING + SQ_SIZE//2 - text.get_width()//2
+        y = i*(SQ_SIZE + PADDING) + PADDING + SQ_SIZE//2 - text.get_height()//2
+        return x, y
+
+    def draw(self, display):
+        pygame.draw.rect(display, DARK_BROWN, (0, 0, WIDTH, HEIGHT-100))
+        for r in range(4):
+            for c in range(4):
+                pygame.draw.rect(display, LIGHT_BROWN, (c*(SQ_SIZE + PADDING) + PADDING, r*(SQ_SIZE + PADDING) + PADDING, SQ_SIZE, SQ_SIZE), 0, 5)
+
+        for i in range(4):
+            for j in range(4):
+                if self.board[i][j]:
+                    color = BLOCK_COLORS[int(math.log(int(self.board[i][j])-1, 2))]
+                    pygame.draw.rect(display, color, (j*(SQ_SIZE + PADDING) + PADDING, i*(SQ_SIZE + PADDING) + PADDING, SQ_SIZE, SQ_SIZE), 0, 5)
+                    if math.log(int(self.board[i][j])-1, 2) <= 4:
+                        text = NUMBER_FONTS[int(math.log(int(self.board[i][j])-1, 2))].render(self.board[i][j], True, LIGHT_TEXT)
+                    else:
+                        text = NUMBER_FONTS[int(math.log(int(self.board[i][j])-1, 2))].render(self.board[i][j], True, DARK_TEXT)
+                    display.blit(text, (self.text_block_loc(text, i, j)[0], self.text_block_loc(text, i, j)[1], SQ_SIZE, SQ_SIZE))
