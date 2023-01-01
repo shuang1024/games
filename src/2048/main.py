033783429a8a4922d7f406cf5dd09c6a42a57d98
Sean Huang <awesome.sean.haha@gmail.com>
2022-12-31 21:52:59 -0800

    Draws basic game board.

--- src/2048/main.py
+++ src/2048/main.py
@@ -21,5 +21,11 @@ def main():
                 pygame.quit()
                 return
 
-        
+        display.fill(WHITE)
+        pygame.draw.rect(display, DARK_BROWN, (0, 0, WIDTH, HEIGHT-100))
+        for r in range(4):
+            for c in range(4):
+                pygame.draw.rect(display, LIGHT_BROWN, (c*(SQ_SIZE + PADDING) + PADDING, r*(SQ_SIZE + PADDING) + PADDING, SQ_SIZE, SQ_SIZE), 0, 5)
+
+
 main()
