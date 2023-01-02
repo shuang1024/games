5fe48e852d0b5a10e1cdb7c29204d33638ecb9d8
Sean Huang <awesome.sean.haha@gmail.com>
2023-01-01 17:57:51 -0800

    Added moving functions using arrow keys.

--- src/2048/board.py
+++ src/2048/board.py
@@ -1,5 +1,6 @@
 import random
 import math
+from copy import deepcopy
 
 import pygame
 
@@ -20,19 +21,67 @@ class Board:
             y2 = random.randint(0, 3)
         self.board[y1][x1] = random.choice(("2", "4"))
         self.board[y2][x2] = "2"
-        
 
     def reset_board(self):
         self.board = []
         for i in range(4):
             self.board.append([])
             for j in range(4):
-                self.board[i].append("")
+                self.board[i].append("0")
+
+    def spawn_block(self):
+        x = random.randint(0, 3)
+        y = random.randint(0, 3)
+        while self.board[y][x] != "0":
+            x = random.randint(0, 3)
+            y = random.randint(0, 3)
+        self.board[y][x] = "2"
 
     def text_block_loc(self, text, i, j):
         x = j*(SQ_SIZE + PADDING) + PADDING + SQ_SIZE//2 - text.get_width()//2
         y = i*(SQ_SIZE + PADDING) + PADDING + SQ_SIZE//2 - text.get_height()//2
         return x, y
+
+    def move_step(self, direction):
+        if direction == "r":
+            for i in range(4):
+                for j in range(4):
+                    if j < 3 and self.board[i][j] != "0":
+                        while self.board[i][j+1] == "0":
+                            self.board[i][j], self.board[i][j+1] = self.board[i][j+1], self.board[i][j]
+
+        if direction == "l":
+            for i in range(4):
+                for j in reversed(range(4)):
+                    if j > 0 and self.board[i][j] != "0":
+                        while self.board[i][j-1] == "0":
+                            self.board[i][j], self.board[i][j-1] = self.board[i][j-1], self.board[i][j]
+
+        if direction == "u":
+            for i in reversed(range(4)):
+                for j in range(4):
+                    if i > 0 and self.board[i][j] != "0":
+                        while self.board[i-1][j] == "0":
+                            self.board[i][j], self.board[i-1][j] = self.board[i-1][j], self.board[i][j]
+
+        if direction == "d":
+            for i in range(4):
+                for j in range(4):
+                    if i < 3 and self.board[i][j] != "0":
+                        while self.board[i+1][j] == "0":
+                            self.board[i][j], self.board[i+1][j] = self.board[i+1][j], self.board[i][j]
+
+    def move(self, direction):
+        revs = 0
+        while True:
+            prev_brd = deepcopy(self.board)
+            self.move_step(direction)
+            if self.board == prev_brd:
+                break
+            revs += 1
+
+        if revs:
+            self.spawn_block()
 
     def draw(self, display):
         pygame.draw.rect(display, DARK_BROWN, (0, 0, WIDTH, HEIGHT-100))
@@ -42,7 +91,7 @@ class Board:
 
         for i in range(4):
             for j in range(4):
-                if self.board[i][j]:
+                if bool(int(self.board[i][j])):
                     color = BLOCK_COLORS[int(math.log(int(self.board[i][j])-1, 2))]
                     pygame.draw.rect(display, color, (j*(SQ_SIZE + PADDING) + PADDING, i*(SQ_SIZE + PADDING) + PADDING, SQ_SIZE, SQ_SIZE), 0, 5)
                     if math.log(int(self.board[i][j])-1, 2) <= 4:
--- src/2048/main.py
+++ src/2048/main.py
@@ -4,6 +4,7 @@ import pygame
 pygame.init()
 
 from constants import *
+from board import Board
 
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048")
@@ -11,21 +12,26 @@ pygame.display.set_caption("2048")
 
 def main():
     clock = pygame.time.Clock()
+    board = Board()
 
     while True:
         clock.tick(FPS)
         pygame.display.update()
         events = pygame.event.get()
+        keys = pygame.key.get_pressed()
         for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+            if event.type == pygame.KEYDOWN:
+                match event.key:
+                    case pygame.K_RIGHT: board.move("r")
+                    case pygame.K_LEFT: board.move("l")
+                    case pygame.K_UP: board.move("u")
+                    case pygame.K_DOWN: board.move("d")
 
         display.fill(WHITE)
-        pygame.draw.rect(display, DARK_BROWN, (0, 0, WIDTH, HEIGHT-100))
-        for r in range(4):
-            for c in range(4):
-                pygame.draw.rect(display, LIGHT_BROWN, (c*(SQ_SIZE + PADDING) + PADDING, r*(SQ_SIZE + PADDING) + PADDING, SQ_SIZE, SQ_SIZE), 0, 5)
+        board.draw(display)
 
 
 main()
